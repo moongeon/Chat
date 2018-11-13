e50bc53ad93f2f8256e4434d6388238bb8830af4
DESKTOP-86L6GGN\pyh92 <htko15@naver.com>
2018-11-13 23:35:11 +0900

    Revert "Revert "second""
    This reverts commit b463a1a15c62baf80f337cdd64e87b773fe32bb4.

--- Chat Client/src/application/Main.java
+++ Chat Client/src/application/Main.java
@@ -18,14 +18,10 @@ import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
 
 
-
-
-
-
 public class Main extends Application {
 
 Socket socket;
-TextArea textArea;
+TextArea textArea ;
 
 //클라이언트 프로그램 동작 메소드
 
@@ -90,9 +86,7 @@ public void receive() {
 		Platform.runLater(()->
 		{
 			textArea.appendText(message);
-		});
-		
-		
+		});	
 		} catch (Exception e) {
                     StopClient();
                     break;
@@ -122,9 +116,9 @@ public void receive() {
 		hbox.getChildren().addAll(userName,IPText,portText);
 		root.setTop(hbox);
 		
-		TextArea textarea = new TextArea();
-		textarea.setEditable(false);
-		root.setCenter(textarea);
+		textArea = new TextArea();
+		textArea.setEditable(false);
+		root.setCenter(textArea);
 		
 		TextField input = new TextField();
 		input.setPrefWidth(Double.MAX_VALUE);
@@ -162,7 +156,7 @@ public void receive() {
 			}
 				StartClient(IPText.getText(), port);
 			    Platform.runLater(()->{
-			    	textarea.appendText("[채팅방 접속]\n");
+			    	textArea.appendText("[채팅방 접속]\n");
 			    	  connectionButton.setText("종료하기");
 			    });
 		
@@ -170,7 +164,7 @@ public void receive() {
 		}else {
 			StopClient();
 		Platform.runLater(()->{
-	    	textarea.appendText("[채팅방 퇴장]\n");
+			textArea.appendText("[채팅방 퇴장]\n");
 	    	connectionButton.setText("접속하기");
 		});
 	
